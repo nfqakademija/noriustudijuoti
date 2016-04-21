aecb79c4f55a3390c24155286a324ed971da8890
Gytis Valukonis <gytis.valukonis@stud.viko.lt>
2016-04-21 13:36:12 +0300

    Added DependencyInjection to AppBundle for crawler service implementation

---
+++ src/AppBundle/DependencyInjection/AppExtension.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gv
+ * Date: 4/21/16
+ * Time: 1:20 PM
+ */
+namespace AppBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class AppExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+
+        $loader->load('services.yml');
+    }
+
+    public function getAlias()
+    {
+        return 'app';
+    }
+}
