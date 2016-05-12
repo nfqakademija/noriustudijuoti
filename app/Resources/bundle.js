64d3aac72ee99818910166af671e8eb1bf989a7a
Gytis Valukonis <gytis.valukonis@stud.viko.lt>
2016-05-12 03:31:54 +0300

    Added sliders

--- app/Resources/bundle.js
+++ app/Resources/bundle.js
@@ -2,3 +2,22 @@ import 'bower_components/bootstrap-sass/assets/javascripts/bootstrap';
 
 import './style/main.scss';
 import './scripts/main';
+
+var rangeSlider = function(){
+    var slider = $('.range-slider'),
+        range = $('.range-slider__range'),
+        value = $('.range-slider__value');
+
+    slider.each(function(){
+
+        value.each(function(){
+            var value = $(this).prev().attr('value');
+            $(this).html(value);
+        });
+
+        range.on('input', function(){
+            $(this).next(value).html(this.value);
+        });
+    });
+};
+rangeSlider();
